c0363e09c97e1feb61d3feaf49e5813b8e9ddb8a
taivox <taivox@gmail.com>
2023-04-07 18:07:14 +0300

    style

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,54 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
-import Feed from './components/main/Feed';
-import Friends from './components/main/Friends';
-import Group from './components/Group';
-import Groups from './components/main/Groups';
-import Profile from './components/Profile';
-import GroupEvents from './components/group/GroupEvents';
-import GroupPosts from './components/group/GroupPosts';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Feed from './components/main/Feed'
+import Friends from './components/main/Friends'
+import Group from './components/Group'
+import Groups from './components/main/Groups'
+import Profile from './components/Profile'
+import GroupEvents from './components/group/GroupEvents'
+import GroupPosts from './components/group/GroupPosts'
+
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>,
+    path: "/",
+    element: <App />,
     children: [
-      {index:true,element:<Feed/>},
+      { index: true, element: <Feed /> },
       {
-        path:"/friends",
-        element:<Friends/>,
+        path: "/friends",
+        element: <Friends />,
       },
       {
-        path:"/groups",
-        element:<Groups/>,
+        path: "/groups",
+        element: <Groups />,
       },
     ]
   },
   {
-    path:"/profile/:id",
-    element:<Profile/>,
+    path: "/profile/:id",
+    element: <Profile />,
   },
   {
-    path:"/groups/:id",
-    element:<Group/>,
+    path: "/groups/:id",
+    element: <Group />,
     children: [
       {
-        path:"events",
-        element:<GroupEvents/>,
+        path: "events",
+        element: <GroupEvents />,
       },
       {
-        path:"",
-        element:<GroupPosts/>,
+        path: "",
+        element: <GroupPosts />,
       },
     ]
   },
 ])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
