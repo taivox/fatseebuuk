99ae95f60c62d953c7e7426006550e79235e1957
taivox <taivox@gmail.com>
2023-04-08 19:53:49 +0300

    event page

--- front-end/src/components/group/GroupEvent.js
+++ front-end/src/components/group/GroupEvent.js
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react"
 import { Link, useOutletContext, useParams } from "react-router-dom"
 import TextArea from "../form/TextArea"
 import PostImagePopup from "../main/PostImagePopup"
+import dateFormat from "../../Utils"
+
 
 function GroupEvent() {
 
 
     const { group_id, event_id } = useParams()
 
-    const [event, setEvent] = useState([])
+    const [event, setEvent] = useState({})
+    const [poster, setPoster] = useState({})
     const [showFullText, setShowFullText] = useState({})
 
     const textLimit = 100
@@ -25,8 +28,10 @@ function GroupEvent() {
         fetch(`${process.env.REACT_APP_BACKEND}/groups/${group_id}/events/${event_id}`, requestOptions)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data)
+                console.log("event", data)
+                console.log("temaongiposter", data.poster)
                 setEvent(data)
+                setPoster(data.poster)
             })
             .catch((error) => {
                 console.log(error)
@@ -45,7 +50,65 @@ function GroupEvent() {
 
     return (
         <div className="col-md-12">
-            {event.title}
+            <div key={event.event_id} className="card">
+                <div className="card-body">
+                    <div className="media ">
+                        <h1>{event.title}</h1>
+                        <p style={{ color: 'red', fontSize: '14px' }}><strong>{`${new Date(event.event_date).toLocaleString('en-UK', dateFormat).slice(0, -3)}`}</strong></p>
+                        <div className="d-flex align-items-center m-2">
+                            <p>Created by: </p>
+                            <Link to={`/profile/${poster.user_id}`}>
+                                <img
+                                    src={`/profile/${poster.profile_picture}`}
+                                    className="mr-3 rounded-circle"
+                                    style={{
+                                        height: "40px",
+                                        width: "40px",
+                                        objectFit: "cover",
+                                        cursor: "pointer"
+                                    }}
+                                    alt="..."
+                                />
+                            </Link>
+                            <div className="m-3">
+                                <h5 className="mt-0" style={{ cursor: "pointer" }}>
+                                    <Link className="Link" to={`/profile/1`}>
+                                        {`${poster.first_name} ${poster.last_name}`}{" "}
+                                    </Link>
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="media-body">
+                            <p className="card-text">
+                                {event.description}
+                            </p>
+                            <img
+                                src={`/event/${event.image}`}
+                                className="img-fluid mb-2"
+                                style={{
+                                    height: "300px",
+                                    width: "600px",
+                                    objectFit: "cover",
+                                    cursor: "pointer"
+                                }}
+                                alt="..."
+                            />
+                            <hr />
+                            <div className="d-flex justify-content-between align-items-center m-2">
+                                <button className="btn btn-light">
+                                    <box-icon name="calendar" /> Going
+                                </button>
+                                <button
+
+                                    className="btn btn-light"
+                                >
+                                    <box-icon name="x" /> Not Going
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
