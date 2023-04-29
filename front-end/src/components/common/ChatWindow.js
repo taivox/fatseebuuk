313db7f132871d01dab0a7f2eabc00f36a4933be
taivox <taivox@gmail.com>
2023-04-30 01:28:19 +0300

    emojis working. need refactoring

--- front-end/src/components/common/ChatWindow.js
+++ front-end/src/components/common/ChatWindow.js
@@ -11,6 +11,58 @@ function ChatWindow({ show, setShow, cookie, friends, selectedChat }) {
   const currentUser = parseInt(localStorage.getItem('user'))
   const [socket, setSocket] = useState(null)
 
+  const regex = /(:\)|:\(|:D|;\)|:P|:O|:\||:\/|:3|;\(|:\*|XD|:'\)|>: \(|8\)|8\(|:\]|:\}|:{|:\\|:s|:\$|:#|:x|:!|:\^ \)|:smirk:|:heart:|:star:|:exclamation:|:question:|:arrow_up:|:arrow_down:|:arrow_left:|:arrow_right:|:ok_hand:|:thumbsup:|:thumbsdown:|:clap:|:pray:|:muscle:|:poop:|:bomb:|:fire:|:100:)/g
+  // Regular expression to match common emojis
+  const unicodeEmojis = {
+    ":)": String.fromCodePoint(0x1F600),
+    ":(": String.fromCodePoint(0x1F62D),
+    ":D": String.fromCodePoint(0x1F603),
+    ";)": String.fromCodePoint(0x1F609),
+    ":P": String.fromCodePoint(0x1F61B),
+    ":O": String.fromCodePoint(0x1F62E),
+    ":|": String.fromCodePoint(0x1F610),
+    ":/": String.fromCodePoint(0x1F615),
+    ":3": String.fromCodePoint(0x1F63A),
+    ";(": String.fromCodePoint(0x1F622),
+    ":*": String.fromCodePoint(0x1F617),
+    "XD": String.fromCodePoint(0x1F606),
+    ":')": String.fromCodePoint(0x1F602),
+    ">:(": String.fromCodePoint(0x1F620),
+    "8)": String.fromCodePoint(0x1F60E),
+    "8(": String.fromCodePoint(0x1F623),
+    ":]": String.fromCodePoint(0x1F60A),
+    ":}": String.fromCodePoint(0x1F60A),
+    ":{": String.fromCodePoint(0x1F641),
+    ":/": String.fromCodePoint(0x1F615),
+    ":\\": String.fromCodePoint(0x1F615),
+    ":s": String.fromCodePoint(0x1F616),
+    ":$": String.fromCodePoint(0x1F633),
+    ":#": String.fromCodePoint(0x1F645),
+    ":x": String.fromCodePoint(0x1F636),
+    ":!": String.fromCodePoint(0x1F631),
+    ":^)": String.fromCodePoint(0x1F60F),
+    ":smirk:": String.fromCodePoint(0x1F60F),
+    ":heart:": String.fromCodePoint(0x2764),
+    ":star:": String.fromCodePoint(0x2B50),
+    ":exclamation:": String.fromCodePoint(0x2757),
+    ":question:": String.fromCodePoint(0x2753),
+    ":arrow_up:": String.fromCodePoint(0x2B06),
+    ":arrow_down:": String.fromCodePoint(0x2B07),
+    ":arrow_left:": String.fromCodePoint(0x2B05),
+    ":arrow_right:": String.fromCodePoint(0x27A1),
+    ":ok_hand:": String.fromCodePoint(0x1F44C),
+    ":thumbsup:": String.fromCodePoint(0x1F44D),
+    ":thumbsdown:": String.fromCodePoint(0x1F44E),
+    ":clap:": String.fromCodePoint(0x1F44F),
+    ":pray:": String.fromCodePoint(0x1F64F),
+    ":muscle:": String.fromCodePoint(0x1F4AA),
+    ":poop:": String.fromCodePoint(0x1F4A9),
+    ":bomb:": String.fromCodePoint(0x1F4A3),
+    ":fire:": String.fromCodePoint(0x1F525),
+    ":100:": String.fromCodePoint(0x1F4AF)
+  } // Map of Unicode emojis for each matched emoji
+
+
   useEffect(() => {
     if (show) {
       const newSocket = new WebSocket('ws://localhost:8080/ws')
@@ -118,7 +170,8 @@ function ChatWindow({ show, setShow, cookie, friends, selectedChat }) {
                         )
                           .map((message) => (
                             <ListGroup.Item key={message.message_id} style={{ wordBreak: "break-all" }}>
-                              <strong>{message.from_id === currentUser ? "You" : selectedFriend.friend.first_name}</strong>: {message.content}
+                              <strong>{message.from_id === currentUser ? "You" : selectedFriend.friend.first_name}</strong>:
+                              {message.content.replace(regex, (match) => unicodeEmojis[match])}
                             </ListGroup.Item>
                           ))}
                     </ListGroup>
@@ -149,7 +202,3 @@ function ChatWindow({ show, setShow, cookie, friends, selectedChat }) {
 export default ChatWindow
 
 
-// .filter((message) =>
-// (message.from_id === currentUser && message.to_id === selectedFriend.user_id) ||
-// (message.from_id === selectedFriend.id && message.to_id === currentUser)
-// )
--- front-end/src/components/group/GroupPosts.js
+++ front-end/src/components/group/GroupPosts.js
@@ -1,11 +1,10 @@
 import { useEffect, useState, useRef } from "react"
-import { Link, Navigate, useNavigate, useOutletContext } from "react-router-dom"
+import { Link, useNavigate, useOutletContext } from "react-router-dom"
 import TextArea from "../form/TextArea"
 import PostImagePopup from "../main/PostImagePopup"
 import Swal from "sweetalert2"
 import { getTimeElapsedString } from "../../Utils"
 import { Row, Col, ListGroup, Form, Button } from 'react-bootstrap'
-import React, { useMemo } from 'react' //emoji
 
 function GroupPosts() {
   const { groupPosts, cookie, group_id, fetchGroup, group } = useOutletContext()
@@ -27,7 +26,57 @@ function GroupPosts() {
   const chatContainerRef = useRef(null)
   const [socket, setSocket] = useState(null)
 
-  const emoji = useMemo(() => String.fromCodePoint(0x1F600), []) //emoji
+  const regex = /(:\)|:\(|:D|;\)|:P|:O|:\||:\/|:3|;\(|:\*|XD|:'\)|>: \(|8\)|8\(|:\]|:\}|:{|:\\|:s|:\$|:#|:x|:!|:\^ \)|:smirk:|:heart:|:star:|:exclamation:|:question:|:arrow_up:|:arrow_down:|:arrow_left:|:arrow_right:|:ok_hand:|:thumbsup:|:thumbsdown:|:clap:|:pray:|:muscle:|:poop:|:bomb:|:fire:|:100:)/g
+  // Regular expression to match common emojis
+  const unicodeEmojis = {
+    ":)": String.fromCodePoint(0x1F600),
+    ":(": String.fromCodePoint(0x1F62D),
+    ":D": String.fromCodePoint(0x1F603),
+    ";)": String.fromCodePoint(0x1F609),
+    ":P": String.fromCodePoint(0x1F61B),
+    ":O": String.fromCodePoint(0x1F62E),
+    ":|": String.fromCodePoint(0x1F610),
+    ":/": String.fromCodePoint(0x1F615),
+    ":3": String.fromCodePoint(0x1F63A),
+    ";(": String.fromCodePoint(0x1F622),
+    ":*": String.fromCodePoint(0x1F617),
+    "XD": String.fromCodePoint(0x1F606),
+    ":')": String.fromCodePoint(0x1F602),
+    ">:(": String.fromCodePoint(0x1F620),
+    "8)": String.fromCodePoint(0x1F60E),
+    "8(": String.fromCodePoint(0x1F623),
+    ":]": String.fromCodePoint(0x1F60A),
+    ":}": String.fromCodePoint(0x1F60A),
+    ":{": String.fromCodePoint(0x1F641),
+    ":/": String.fromCodePoint(0x1F615),
+    ":\\": String.fromCodePoint(0x1F615),
+    ":s": String.fromCodePoint(0x1F616),
+    ":$": String.fromCodePoint(0x1F633),
+    ":#": String.fromCodePoint(0x1F645),
+    ":x": String.fromCodePoint(0x1F636),
+    ":!": String.fromCodePoint(0x1F631),
+    ":^)": String.fromCodePoint(0x1F60F),
+    ":smirk:": String.fromCodePoint(0x1F60F),
+    ":heart:": String.fromCodePoint(0x2764),
+    ":star:": String.fromCodePoint(0x2B50),
+    ":exclamation:": String.fromCodePoint(0x2757),
+    ":question:": String.fromCodePoint(0x2753),
+    ":arrow_up:": String.fromCodePoint(0x2B06),
+    ":arrow_down:": String.fromCodePoint(0x2B07),
+    ":arrow_left:": String.fromCodePoint(0x2B05),
+    ":arrow_right:": String.fromCodePoint(0x27A1),
+    ":ok_hand:": String.fromCodePoint(0x1F44C),
+    ":thumbsup:": String.fromCodePoint(0x1F44D),
+    ":thumbsdown:": String.fromCodePoint(0x1F44E),
+    ":clap:": String.fromCodePoint(0x1F44F),
+    ":pray:": String.fromCodePoint(0x1F64F),
+    ":muscle:": String.fromCodePoint(0x1F4AA),
+    ":poop:": String.fromCodePoint(0x1F4A9),
+    ":bomb:": String.fromCodePoint(0x1F4A3),
+    ":fire:": String.fromCodePoint(0x1F525),
+    ":100:": String.fromCodePoint(0x1F4AF)
+  } // Map of Unicode emojis for each matched emoji
+
 
   const handleImageClick = (post, index) => {
     setSelectedPost(post)
@@ -292,13 +341,8 @@ function GroupPosts() {
                 {messages && messages.length > 0 &&
                   messages.map((message) => (
                     <ListGroup.Item key={message.message_id} style={{ wordBreak: "break-all" }}>
-                      <strong>{message.from_id === currentUser.user_id ? "You" : (() => getSenderName(message.from_id))()}</strong>: {message.content}
-
-                      <div>
-                        <span role="img" >asdasdasdasdasd{emoji}kjaasdasd</span>
-                        {/* emoji */}
-                      </div>
-
+                      <strong>{message.from_id === currentUser.user_id ? "You" : (() => getSenderName(message.from_id))()}</strong>:
+                      {message.content.replace(regex, (match) => unicodeEmojis[match])}
                     </ListGroup.Item>
                   ))}
               </ListGroup>
